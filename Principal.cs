838ec2f6a52aaf512d63eb4c076fe9c4a0b9257a
jfr1982 <jfr1982@d4da03dc-e410-ddcd-3da0-c4579476d185>
2010-09-09 20:02:32 +0000

    + Subida primera version

---
+++ Principal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OnlineRadioTunner
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+           
+            notIcon.Visible = true;
+
+            notIcon.ContextMenu = ctxMenu;
+        }
+
+        private void wMediaPlayer_StatusChange(object sender, EventArgs e)
+        {
+            AxWMPLib.AxWindowsMediaPlayer wmp = (AxWMPLib.AxWindowsMediaPlayer)sender;
+            notIcon.ShowBalloonTip(30000000, "Emisora", wmp.status, ToolTipIcon.Info);
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            wMediaPlayer.URL = "mms://reflector.marca.com:9054";
+        }
+
+        private void menuItem4_Click(object sender, EventArgs e)
+        {
+            wMediaPlayer.URL = "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_sevilla";
+        }
+    }
+}
