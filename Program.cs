e5b876ccaf80c70c81586f07be47cb3f65acc1da
jfr1982 <jfr1982@d4da03dc-e410-ddcd-3da0-c4579476d185>
2010-09-10 07:05:11 +0000

    # Modificado para que las emisoras sean configurables

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace OnlineRadioTunner
 {
@@ -16,8 +19,36 @@ namespace OnlineRadioTunner
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Principal pfrm = new Principal();
-            //pfrm.Hide();
+
+
+            /*
+            OnlineRadioTunnerSystem system = new OnlineRadioTunnerSystem();
+            RadioStationGroup group = new RadioStationGroup("Onda Cero");
+            group.Stations.Add(new RadioStation("OC - Sevilla", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_sevilla"));
+            group.Stations.Add(new RadioStation("OC - Almeria", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_almeria"));
+
+            system.Groups.Add(group);
+            
+            RadioStationGroup group2 = new RadioStationGroup("Marca");
+            group2.Stations.Add(new RadioStation("M - Sevilla", "mms://reflector.marca.com:9054"));
+
+            system.Groups.Add(group2);
+
+
+            XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
+
+           
+            StreamWriter w = new System.IO.StreamWriter("rstation.xml");
+            serializer.Serialize(w, system);
+            w.Close();
+            */
+
+            XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
+            StreamReader r = new StreamReader("rstation.xml");
+            OnlineRadioTunnerSystem system = (OnlineRadioTunnerSystem)serializer.Deserialize(r);            
+
+
+            Principal pfrm = new Principal(system);
             Application.Run();
         }
     }
