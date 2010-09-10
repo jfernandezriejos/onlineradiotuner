f09d856b1f18ba26a7bee485414fe5737ed8a5d1
jfr1982 <jfr1982@d4da03dc-e410-ddcd-3da0-c4579476d185>
2010-09-10 07:18:14 +0000

    + Añadidas mas emisoras

--- trunk/Program.cs
+++ trunk/Program.cs
@@ -21,32 +21,37 @@ namespace OnlineRadioTunner
 
 
 
-            /*
-            OnlineRadioTunnerSystem system = new OnlineRadioTunnerSystem();
+            /* // PEQUEÑO TROZO DE CODIGO PARA SERIALIZAR LAS EMISORAS
+			OnlineRadioTunnerSystem system = new OnlineRadioTunnerSystem();
             RadioStationGroup group = new RadioStationGroup("Onda Cero");
             group.Stations.Add(new RadioStation("OC - Sevilla", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_sevilla"));
             group.Stations.Add(new RadioStation("OC - Almeria", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_almeria"));
-
-            system.Groups.Add(group);
+			system.Groups.Add(group);
             
             RadioStationGroup group2 = new RadioStationGroup("Marca");
             group2.Stations.Add(new RadioStation("M - Sevilla", "mms://reflector.marca.com:9054"));
 
-            system.Groups.Add(group2);
-
+			system.Groups.Add(group2);
+			
+			RadioStationGroup group3 = new RadioStationGroup("Canal Sur Radio");
+            group3.Stations.Add(new RadioStation("CSR", "http://canalsurradio.rtva.stream.flumotion.com/rtva/canalsurradio_master.mp3.m3u"));
+			system.Groups.Add(group3);
+			
+			RadioStationGroup group4 = new RadioStationGroup("Rock and Gol");
+            group4.Stations.Add(new RadioStation("R&G", "http://rockandgol.cope.stream.flumotion.com/cope/rockandgol.asf.asx"));
+			system.Groups.Add(group4);
+            
 
             XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
 
            
             StreamWriter w = new System.IO.StreamWriter("rstation.xml");
             serializer.Serialize(w, system);
-            w.Close();
-            */
+            w.Close();*/
 
             XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
             StreamReader r = new StreamReader("rstation.xml");
-            OnlineRadioTunnerSystem system = (OnlineRadioTunnerSystem)serializer.Deserialize(r);            
-
+            OnlineRadioTunnerSystem system = (OnlineRadioTunnerSystem)serializer.Deserialize(r); 
 
             Principal pfrm = new Principal(system);
             Application.Run();
---
+++ trunk/RadioStation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OnlineRadioTunner
+{
+    
+    
+    [XmlRoot(ElementName="RadioStation")]
+    public class RadioStation
+    {
+        public String Name { get; set; }
+        public String Url { get; set; }
+
+        public RadioStation()
+        { }
+
+        public RadioStation(String name, String url)
+        {
+            Name = name;
+            Url = url;
+        }
+    }
+
+    [XmlRoot(ElementName = "RadioStationGroup")]
+    public class RadioStationGroup
+    {
+        public String Name { get; set; }
+        public List<RadioStation> Stations { get; set; }
+
+        public RadioStationGroup()
+        {
+            Stations = new List<RadioStation>();
+        }
+        
+        public RadioStationGroup(String name)
+        {
+            Name = name;
+            Stations = new List<RadioStation>();
+        }
+    }
+
+    [XmlRoot(ElementName = "OnlineRadioTunner")]
+    public class OnlineRadioTunnerSystem
+    {
+        public List<RadioStationGroup> Groups { get; set; }
+
+        public OnlineRadioTunnerSystem()
+        {
+            Groups = new List<RadioStationGroup>();
+        }
+    }
+}
