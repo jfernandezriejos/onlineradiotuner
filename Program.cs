1da4cb93c46bf1f4164deebc8fbd8df3701a0d69
jfr1982 <jfr1982@d4da03dc-e410-ddcd-3da0-c4579476d185>
2010-09-26 19:46:34 +0000

    # Soporte parcial de flash

--- Program.cs
+++ Program.cs
@@ -19,54 +19,64 @@ namespace OnlineRadioTunner
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-
-
-            /* 
             // PEQUEÑO TROZO DE CODIGO PARA SERIALIZAR LAS EMISORAS
 			OnlineRadioTunnerSystem system = new OnlineRadioTunnerSystem();
             RadioStationGroup group = new RadioStationGroup("Onda Cero");
-            group.Stations.Add(new RadioStation("OC - Sevilla", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_sevilla"));
-            group.Stations.Add(new RadioStation("OC - Almeria", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_almeria"));
+            group.Stations.Add(new RadioStation("OC - Sevilla", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_sevilla", StationType.WMP));
+            group.Stations.Add(new RadioStation("OC - Almeria", "mms://ondacerolivewm.fplive.net/ondacerowmlive-live/oc_almeria", StationType.WMP));
 			system.Groups.Add(group);
             
             RadioStationGroup group2 = new RadioStationGroup("Marca");
-            group2.Stations.Add(new RadioStation("M - Sevilla", "mms://reflector.marca.com:9054"));
+            group2.Stations.Add(new RadioStation("M - Sevilla", "mms://reflector.marca.com:9054", StationType.WMP));
 
 			system.Groups.Add(group2);
 			
 			RadioStationGroup group3 = new RadioStationGroup("Canal Sur Radio");
-            group3.Stations.Add(new RadioStation("CSR", "http://canalsurradio.rtva.stream.flumotion.com/rtva/canalsurradio_master.mp3.m3u"));
+            group3.Stations.Add(new RadioStation("CSR", "http://canalsurradio.rtva.stream.flumotion.com/rtva/canalsurradio_master.mp3.m3u", StationType.WMP));
 			system.Groups.Add(group3);
 			
 			RadioStationGroup group4 = new RadioStationGroup("Rock and Gol");
-            group4.Stations.Add(new RadioStation("R&G", "http://rockandgol.cope.stream.flumotion.com/cope/rockandgol.asf.asx"));
+            group4.Stations.Add(new RadioStation("R&G", "http://rockandgol.cope.stream.flumotion.com/cope/rockandgol.asf.asx", StationType.WMP));
 			system.Groups.Add(group4);
             
             RadioStationGroup group5 = new RadioStationGroup("RNE");
-            group5.Stations.Add(new RadioStation("Radio Nacional", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3348_es_audios&location=radio&volume=0.75&autostart=true"));
-            group5.Stations.Add(new RadioStation("Radio Clásica", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3343_es_audios&location=radio&volume=0.75&autostart=true"));
-            group5.Stations.Add(new RadioStation("Radio 3", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3342_es_audios&location=radio&volume=0.75&autostart=true"));
-            group5.Stations.Add(new RadioStation("Radio 4", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3344_es_audios&location=radio&volume=0.75&autostart=true"));
-            group5.Stations.Add(new RadioStation("Radio 5 Todo Noticias", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3345_es_audios&location=radio&volume=0.75&autostart=true"));
-            group5.Stations.Add(new RadioStation("Radio Exterior", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3346_es_audios&location=radio&volume=0.75&autostart=true"));
-            
-
+            group5.Stations.Add(new RadioStation("Radio Nacional", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3348_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
+            group5.Stations.Add(new RadioStation("Radio Clásica", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3343_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
+            group5.Stations.Add(new RadioStation("Radio 3", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3342_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
+            group5.Stations.Add(new RadioStation("Radio 4", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3344_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
+            group5.Stations.Add(new RadioStation("Radio 5 Todo Noticias", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3345_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
+            group5.Stations.Add(new RadioStation("Radio Exterior", "http://www.rtve.es/swf/3.0.33/RTVEPlayer.swf?assetID=3346_es_audios&location=radio&volume=0.75&autostart=true", StationType.WMP));
             system.Groups.Add(group5);
 
-            XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
+            RadioStationGroup group6 = new RadioStationGroup("Cadena Ser");
+            group6.Stations.Add(new RadioStation("CS - Sevilla", "http://194.169.201.177:8085/stream/prisacom_Sevilla", StationType.WMP));
+			system.Groups.Add(group6);
 
+            RadioStationGroup group7 = new RadioStationGroup("Cadena100");
+            group7.Stations.Add(new RadioStation("C100", "http://cadena100.cope.stream.flumotion.com/cope/cadena100.flv.m3u", StationType.WMP));
+			system.Groups.Add(group7);
+
+            RadioStationGroup group8 = new RadioStationGroup("Europa FM");
+            group8.Stations.Add(new RadioStation("EFM", "http://www.europafm.com/EuropaFM/swf/PlayerEFM-OSMF.swf?var1=http://www.antena3.com/player/europafm_streams.xml", StationType.WMP));
+			system.Groups.Add(group8);
+
+            RadioStationGroup group9 = new RadioStationGroup("Los 40");
+            group9.Stations.Add(new RadioStation("40", " http://player.streamtheworld.com/_players/unionradio/?callsign=LOS40&streamid=3993&channelid=", StationType.FLASH));
+            system.Groups.Add(group9);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
            
             StreamWriter w = new System.IO.StreamWriter("rstation.xml");
             serializer.Serialize(w, system);
             w.Close();
-            */
 
-            XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
+            /*XmlSerializer serializer = new XmlSerializer(typeof(OnlineRadioTunnerSystem));
             StreamReader r = new StreamReader("rstation.xml");
-            OnlineRadioTunnerSystem system = (OnlineRadioTunnerSystem)serializer.Deserialize(r); 
+            OnlineRadioTunnerSystem system = (OnlineRadioTunnerSystem)serializer.Deserialize(r);
+            r.Close();*/
 
             Principal pfrm = new Principal(system);
-            Application.Run();
+            Application.Run(pfrm);
         }
     }
 }
